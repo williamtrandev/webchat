852027bf6bec7cb536272f12c7c2f9637cc49ac9
WilliamTran2k3 <trantanthanh9a10@gmail.com>
2023-01-16 22:30:29 +0700

    new

--- server/index.js
+++ server/index.js
@@ -31,7 +31,10 @@ mongoose
         console.log(err.message);
         console.log(1);
     });
-
+app.get("/:universalURL", (req, res) => {
+    res.send("404 URL NOT FOUND");
+});
+  
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     console.log(`Server started on PORT ${process.env.PORT}`);
